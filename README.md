6fbddd324a45916025b6de3c405229cee4332962
Lael Nascimento Santos <lael.nasan@gmail.com>
2021-11-29 16:55:48 -0400

    start versioning

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring Getting Started guide: Accessing data with JPA
+
+this repository is a short study of the [Spring Guide](https://spring.io/guides/gs/accessing-data-jpa/).
---
+++ src/test/java/br/com/nasan/gsaccessingdatajpa/GsAccessingDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nasan.gsaccessingdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsAccessingDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
